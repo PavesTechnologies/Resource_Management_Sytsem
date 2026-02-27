79dae3d98794b55cb98cb755861554557997602c
VENKATESH0018 <justvenky18@gmail.com>
2026-02-27 18:18:25 +0530

    updated demand conflicts

--- ResourceManagementSystem/src/main/java/com/service_interface/demand_service_interface/DemandService.java
+++ ResourceManagementSystem/src/main/java/com/service_interface/demand_service_interface/DemandService.java
@@ -2,6 +2,7 @@ package com.service_interface.demand_service_interface;
 
 import com.dto.ApiResponse;
 import com.dto.demand_dto.CreateDemandDTO;
+import com.dto.demand_dto.DemandConflictValidationDTO;
 import com.dto.demand_dto.UpdateDemandDTO;
 import com.entity.demand_entities.Demand;
 import org.springframework.http.ResponseEntity;
@@ -16,4 +17,10 @@ public interface DemandService {
     ResponseEntity<ApiResponse<?>> createDemand(CreateDemandDTO dto, Long id);
     ResponseEntity<ApiResponse<?>> getDemandById(UUID demandId);
     ResponseEntity<ApiResponse<?>> getDemandsByResourceManagerId(Long resourceManagerId);
+    
+    // Conflict resolution method
+    ResponseEntity<ApiResponse<?>> resolveDemandConflicts(Long projectId);
+    
+    // Pre-submission validation for early conflict detection
+    ResponseEntity<ApiResponse<DemandConflictValidationDTO>> validateDemandConflicts(CreateDemandDTO dto);
 }
