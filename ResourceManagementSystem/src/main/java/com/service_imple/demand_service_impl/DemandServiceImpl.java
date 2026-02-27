5529b60a0c1e45aaa0dac19b986f3c6c1cecf935
VENKATESH0018 <justvenky18@gmail.com>
2026-02-27 15:58:27 +0530

    updated company escalation

--- ResourceManagementSystem/src/main/java/com/service_imple/demand_service_impl/DemandServiceImpl.java
+++ ResourceManagementSystem/src/main/java/com/service_imple/demand_service_impl/DemandServiceImpl.java
@@ -85,7 +85,7 @@ public class DemandServiceImpl implements DemandService {
             demand.setDemandName(dto.getDemandName());
             demand.setDemandType(dto.getDemandType());
             demand.setDemandStartDate(dto.getDemandStartDate());
-            demand.setDemandEndDate(dto.getDemandEndDate().atStartOfDay());
+            demand.setDemandEndDate(dto.getDemandEndDate());
             demand.setAllocationPercentage(dto.getAllocationPercentage());
             demand.setDeliveryModel(dto.getDeliveryModel());
             demand.setDemandStatus(dto.getDemandStatus());
@@ -181,7 +181,7 @@ public class DemandServiceImpl implements DemandService {
             existing.setDemandJustification(dto.getDemandJustification());
 
         if (dto.getDemandStartDate() != null)
-            existing.setDemandStartDate(dto.getDemandStartDate().toLocalDate());
+            existing.setDemandStartDate(dto.getDemandStartDate());
 
         if (dto.getDemandEndDate() != null)
             existing.setDemandEndDate(dto.getDemandEndDate());
