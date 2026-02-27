5133126549daeb23c86220b1b108eb23290208fa
Aditya <adityateja0118@gmail.com>
2026-02-27 18:24:43 +0530

    auto replacement

---
+++ ResourceManagementSystem/src/main/java/com/repo/allocation_repo/RoleOffEventRepository.java
@@ -0,0 +1,7 @@
+package com.repo.allocation_repo;
+
+import com.entity.allocation_entities.RoleOffEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleOffEventRepository extends JpaRepository<RoleOffEvent, Long> {
+}
