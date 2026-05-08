ea0fa9f51da6db66e6565c37c17218f31b7c3be8
Aditya <adityateja0118@gmail.com>
2026-05-08 16:18:36 +0530

    tested and changed

--- ResourceManagementSystem/src/main/java/com/dto/demand_dto/UpdateDemandDTO.java
+++ ResourceManagementSystem/src/main/java/com/dto/demand_dto/UpdateDemandDTO.java
@@ -24,6 +24,10 @@ public class UpdateDemandDTO {
 
     private Long outgoingResourceId;
 
+    private UUID deliveryRole;
+
+//    private String deliveryRoleName;
+
     private String demandJustification;
 
     private LocalDate demandStartDate;
