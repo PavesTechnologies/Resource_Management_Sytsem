fc5d5ba213116e749ed50260c75500f97039c162
rangaswamy9999 <damarangaswamy154@gmail.com>
2026-06-10 15:00:02 +0530

    cross origin  dynamic implementation

--- src/main/java/com/config/CorsConfig.java
+++ src/main/java/com/config/CorsConfig.java
@@ -1,24 +1,27 @@
 package com.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOriginsRaw;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://d15j2ej3bear0q.cloudfront.net"
-        ));
+        List<String> allowedOrigins = Arrays.asList(allowedOriginsRaw.split(","));
+        configuration.setAllowedOrigins(allowedOrigins);
 
         configuration.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
